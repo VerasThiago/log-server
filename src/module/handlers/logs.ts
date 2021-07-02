19ff97ef787bdc97db92a0158e00175720b453bc
Thiago Veras <thiago.veras.machado@gmail.com>
2021-07-02 13:56:34 -0300

    lint

--- src/module/handlers/logs.ts
+++ src/module/handlers/logs.ts
@@ -2,18 +2,18 @@ import Koa from 'koa'
 import coBody from 'co-body'
 
 interface Data {
-  time: string,
-  file: string,
-  line: number,
+  time: string
+  file: string
+  line: number
   message: string
 }
 
 export async function logsHandler(ctx: Koa.ParameterizedContext) {
-  
   try {
     const body: Data = await coBody(ctx.req)
+
     console.log(body)
-    ctx.body = "Success"
+    ctx.body = 'Success'
     ctx.status = 200
   } catch (e) {
     ctx.body = e.message
